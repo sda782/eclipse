b8bd24530c7b76bb425b3bd99eb5c6511490f514
sda782 <saerdna.petersen@gmail.com>
2022-05-05 16:42:08 +0200

    sidebar

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
     import { Route } from "svelte-navigator";
     import Home from "./Home.svelte";
+    import Sidebar from "./Sidebar.svelte";
     import YoutubeLive from "./YoutubeApi/YoutubeLive.svelte";
     import YtVideo from "./YoutubeApi/YtVideo.svelte";
 </script>
 
 <div class="container m-0 p-0 w-100" style="max-width: none;">
     <div class="row w-100">
-        <div class="col-2">sidebar</div>
+        <div class="col-2 "><Sidebar /></div>
         <div class="col-10">
             <Route path="/" component={Home} />
             <Route path="/ytl/*" component={YoutubeLive} />
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,11 @@
+<h2 class="ps-5 pe-5">sidebar</h2>
+<ul class="list-group list-group-flush">
+    <li class="list-group-item list-group-item-action">Hello</li>
+    <li class="list-group-item list-group-item-action">Hello</li>
+    <li class="list-group-item list-group-item-action">Hello</li>
+    <li class="list-group-item list-group-item-action">Hello</li>
+    <li class="list-group-item list-group-item-action">Hello</li>
+    <li class="list-group-item list-group-item-action">Hello</li>
+    <li class="list-group-item list-group-item-action">Hello</li>
+    <li class="list-group-item list-group-item-action">Hello</li>
+</ul>
