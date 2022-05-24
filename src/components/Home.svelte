86c97bef7c92ebc243aef0e7f41cdac2f2de8bbf
sda782 <saerdna.petersen@gmail.com>
2022-05-24 20:45:12 +0200

    ayo

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -4,13 +4,14 @@
 
 <div class="">
     <h1>Welcome to eclipse</h1>
-    <h6><i>created by Aevum</i></h6>
+    <h6><i>created by Andreas</i></h6>
     <p>
         This app is created as part of my 4 semester exam. And demostrate how
         svelte can be used to create a Single Page Application.
     </p>
-    <div>
-        <p>Currently the app uses the Youtube Data Api to show top videos</p>
-        <Link to="ytl" class="btn btn-primary">Explore</Link>
-    </div>
+    <p>
+        Currently the app uses the Youtube Data Api to show top videos, as well
+        as let you search for videos
+    </p>
+    <Link to="ytl" class="btn btn-primary">Explore</Link>
 </div>
